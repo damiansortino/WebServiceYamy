500a1e8544e0407fc14ae104fc60bdbeefdc6e16
DAMIAN SORTINO <92537867+damiansortino@users.noreply.github.com>
2023-05-31 10:52:51 -0300

    se quitaron los centavos

---
+++ Servicios/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+
+namespace Servicios
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ Servicios/Controllers/ProductoController.cs
@@ -0,0 +1,47 @@
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Servicios.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        public List<prod> Get()
+        {
+            List<prod> listado = new List<prod>();
+            using (baselaymarEntities db = new baselaymarEntities())
+            {
+                foreach (var item in db.producto)
+                {
+                    if (item.fechaBaja == null)
+                    {
+                        prod nuevoprod = new prod(item);
+                        listado.Add(nuevoprod);
+                    }
+                }
+            }
+            return listado;
+        }
+
+        // GET: api/producto/5
+        public prod Get(int id)
+        {
+            prod devolver = new prod();
+
+            using (baselaymarEntities db = new baselaymarEntities())
+            {
+
+                foreach (var item in db.producto)
+                {
+                    if ((item.fechaBaja == null) && (int.Parse(item.codigo) == id))
+                    {
+                        prod nuevo = new prod(item);
+                        devolver = nuevo;
+                    }
+                }
+            }
+            return devolver;
+        }
+    }
+}
